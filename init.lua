baafe40dded5b95b3e014a2c6f0539c4e2e37d17
John Trump <admin@fuckwechat.com>
2025-05-29 17:31:09 +0800

    Add ModalMgr and WinWin spoons for enhanced window management
    - Introduced ModalMgr for managing modal keybindings environments.
    - Implemented keybindings for window resizing and movement.
    - Added WinWin for window manipulation with grid-based controls.
    - Created documentation for both ModalMgr and WinWin methods and usage.

--- init.lua
+++ init.lua
@@ -47,6 +47,10 @@ end
 
 -- 需要用到 loop 窗口的特性
 hs.hotkey.bind(hyper, "y", switch_to_app("DingTalk", "com.alibaba.DingTalkMac"))
+hs.hotkey.bind(hyper, "f", function()
+    local sf = hs.screen.primaryScreen():frame()
+    hs.window.focusedWindow():setFrame(hs.geometry.new(sf.x, sf.y, sf.w, sf.h))
+end)
 
 --[[ 切换应用 ]]
 hyperKey:bind('x'):toApplication('/Applications/WeChat.app')
@@ -55,7 +59,7 @@ hyperKey:bind('e'):toApplication('/Applications/Cursor.app')
 hyperKey:bind('n'):toApplication('/Applications/Obsidian.app')
 hyperKey:bind('i'):toApplication('/Applications/Warp.app')
 hyperKey:bind('c'):toApplication('/Applications/Cherry Studio.app')
-hyperKey:bind('f'):toApplication('/Applications/Figma.app')
+hyperKey:bind('p'):toApplication('/Applications/Figma.app')
 hyperKey:bind('m'):toApplication('/Applications/iPhone Mirroring.app')
 hyperKey:bind('q'):toApplication('/Applications/Telegram.app')
 
@@ -203,3 +207,76 @@ wifiWatcher:start()
 hs.urlevent.bind("someAlert", function(eventName, params)
     hs.alert.show("Received someAlert")
 end)
+
+
+
+--[[ 视窗管理 ]]
+hs.loadSpoon("ModalMgr")
+hs.loadSpoon("WinWin")
+
+
+if spoon.WinWin then
+    spoon.ModalMgr:new("resizeM")
+    local cmodal = spoon.ModalMgr.modal_list["resizeM"]
+    cmodal:bind('', 'escape', '退出 ', function() spoon.ModalMgr:deactivate({"resizeM"}) end)
+    cmodal:bind('', 'Q', '退出', function() spoon.ModalMgr:deactivate({"resizeM"}) end)
+    cmodal:bind('', 'tab', '键位提示', function() spoon.ModalMgr:toggleCheatsheet() end)
+
+    cmodal:bind('', 'A', '向左移动', function() spoon.WinWin:stepMove("left") end, nil, function() spoon.WinWin:stepMove("left") end)
+    cmodal:bind('', 'D', '向右移动', function() spoon.WinWin:stepMove("right") end, nil, function() spoon.WinWin:stepMove("right") end)
+    cmodal:bind('', 'W', '向上移动', function() spoon.WinWin:stepMove("up") end, nil, function() spoon.WinWin:stepMove("up") end)
+    cmodal:bind('', 'S', '向下移动', function() spoon.WinWin:stepMove("down") end, nil, function() spoon.WinWin:stepMove("down") end)
+
+    cmodal:bind('', 'H', '左半屏', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("halfleft") end)
+    cmodal:bind('', 'L', '右半屏', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("halfright") end)
+    cmodal:bind('', 'K', '上半屏', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("halfup") end)
+    cmodal:bind('', 'J', '下半屏', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("halfdown") end)
+
+    cmodal:bind('', 'Y', '屏幕左上角', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("cornerNW") end)
+    cmodal:bind('', 'O', '屏幕右上角', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("cornerNE") end)
+    cmodal:bind('', 'U', '屏幕左下角', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("cornerSW") end)
+    cmodal:bind('', 'I', '屏幕右下角', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("cornerSE") end)
+
+    cmodal:bind('', 'F', '全屏', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("fullscreen") end)
+    cmodal:bind('', 'C', '居中', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("center") end)
+    cmodal:bind('', 'G', '左三分之二屏居中分屏', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("centermost") end)
+    cmodal:bind('', 'Z', '展示显示', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("show") end)
+    cmodal:bind('', 'V', '编辑显示', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("shows") end)
+
+    cmodal:bind('', 'X', '二分之一居中分屏', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("center-2") end)
+
+    cmodal:bind('', '=', '窗口放大', function() spoon.WinWin:moveAndResize("expand") end, nil, function() spoon.WinWin:moveAndResize("expand") end)
+    cmodal:bind('', '-', '窗口缩小', function() spoon.WinWin:moveAndResize("shrink") end, nil, function() spoon.WinWin:moveAndResize("shrink") end)
+
+    cmodal:bind('ctrl', 'H', '向左收缩窗口', function() spoon.WinWin:stepResize("left") end, nil, function() spoon.WinWin:stepResize("left") end)
+    cmodal:bind('ctrl', 'L', '向右扩展窗口', function() spoon.WinWin:stepResize("right") end, nil, function() spoon.WinWin:stepResize("right") end)
+    cmodal:bind('ctrl', 'K', '向上收缩窗口', function() spoon.WinWin:stepResize("up") end, nil, function() spoon.WinWin:stepResize("up") end)
+    cmodal:bind('ctrl', 'J', '向下扩镇窗口', function() spoon.WinWin:stepResize("down") end, nil, function() spoon.WinWin:stepResize("down") end)
+
+
+    cmodal:bind('', 'left', '窗口移至左边屏幕', function() spoon.WinWin:stash() spoon.WinWin:moveToScreen("left") end)
+    cmodal:bind('', 'right', '窗口移至右边屏幕', function() spoon.WinWin:stash() spoon.WinWin:moveToScreen("right") end)
+    cmodal:bind('', 'up', '窗口移至上边屏幕', function() spoon.WinWin:stash() spoon.WinWin:moveToScreen("up") end)
+    cmodal:bind('', 'down', '窗口移动下边屏幕', function() spoon.WinWin:stash() spoon.WinWin:moveToScreen("down") end)
+    cmodal:bind('', 'space', '窗口移至下一个屏幕', function() spoon.WinWin:stash() spoon.WinWin:moveToScreen("next") end)
+    cmodal:bind('', 'B', '撤销最后一个窗口操作', function() spoon.WinWin:undo() end)
+    cmodal:bind('', 'R', '重做最后一个窗口操作', function() spoon.WinWin:redo() end)
+
+    cmodal:bind('', '[', '左二分之一屏', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("halfleft") end)
+    cmodal:bind('', ']', '右二分之一屏', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("halfright") end)
+
+    cmodal:bind('', 't', '将光标移至所在窗口中心位置', function() spoon.WinWin:centerCursor() end)
+
+    local function enterResizeM()
+        spoon.ModalMgr:deactivateAll()
+        spoon.ModalMgr:activate({"resizeM"}, "#B22222")
+    end
+
+    spoon.ModalMgr.supervisor:bind('ctrl', "space", "进入窗口管理模式", enterResizeM)
+    spoon.ModalMgr.supervisor:bind(hyper, "r", "进入窗口管理模式", enterResizeM)
+    spoon.ModalMgr.supervisor:bind({"cmd", "ctrl"}, "space", "进入窗口管理模式", enterResizeM)
+end
+
+
+
+spoon.ModalMgr.supervisor:enter()
